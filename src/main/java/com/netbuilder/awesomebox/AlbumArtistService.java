f5046651fe7b58a11c9c5528506e81681e4a72b2
cjames1224 <cjames1224@gmail.com>
2014-08-13 17:12:32 +0100

    Added some commented out UD functions to AlbumArtistService

--- src/main/java/com/netbuilder/awesomebox/AlbumArtistService.java
+++ src/main/java/com/netbuilder/awesomebox/AlbumArtistService.java
@@ -3,6 +3,7 @@ package com.netbuilder.awesomebox;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public class AlbumArtistService {
 	
@@ -30,5 +31,22 @@ public class AlbumArtistService {
 			System.out.println(a.toString());
 		}
 	}
+	
+//	public void updateAlbumArtist(AlbumArtist albumArtist,Album album,Artist artist) {
+//		em.getTransaction().begin();
+//		String query = "UPDATE AlbumArtist SET album = \'" + album + "\', artist = " + artist + " WHERE id = " + albumArtist.getId();
+//		em.createQuery(query);
+//		albumArtist.setArtist(artist);
+//		albumArtist.setAlbum(album);
+//		em.getTransaction().commit();
+//	}
+	
+//	public void deleteAlbumArtist(AlbumArtist albumArtist) {
+//		em.getTransaction().begin();
+//		String query = "DELETE FROM AlbumArtist WHERE id = " + albumArtist.getId();
+//		Query q = em.createQuery(query);
+//		q.executeUpdate();
+//		em.getTransaction().commit();
+//	}
 
 }
