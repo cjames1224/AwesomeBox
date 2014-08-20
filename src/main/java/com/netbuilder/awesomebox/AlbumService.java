489b508ede095382f7bb1d92c4e99369fc3fddcb
cjames1224 <cjames1224@gmail.com>
2014-08-20 12:01:20 +0100

    WOrked on DBpopulator, made album generation id type identity, made albumservice into stateless bean

--- src/main/java/com/netbuilder/awesomebox/AlbumService.java
+++ src/main/java/com/netbuilder/awesomebox/AlbumService.java
@@ -4,52 +4,32 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-@Named
-@SessionScoped
+@Stateless
 public class AlbumService implements Serializable {
 	
-	private static final long serialVersionUID = 5443351151396868724L;
 	@Inject
 	private EntityManager em;
-	private List<Album> albumList;
-	
-	public AlbumService(){
-		
-	}
-	
-	@PostConstruct
-	public void updateAlbumList() {
-		List<Album> list = em.createQuery("SELECT a FROM Album a",
-				Album.class).getResultList();
-		this.albumList = list;
-	}
 	
 	public List<Album> getAlbumList() {
-		return albumList;
+		return em.createQuery("SELECT a FROM Album a",
+				Album.class).getResultList();
 	}
 
-	public void setAlbumList(List<Album> albumList) {
-		this.albumList = albumList;
-	}
-
-	public void persistAlbumList(List<Album> list){
+	public void createAlbumList(List<Album> list){
 		if (list == null) {
 			throw new ValidationException("Invalid List");
 		}
 		
-	
-		
 		for(Album a: list){
 			em.persist(a);
 		}
-		
-	
 	}
 	
 	public void listAlbums(){
@@ -86,7 +66,6 @@ public class AlbumService implements Serializable {
 			throw new ValidationException("Invalid Album Update");
 		}
 		
-
 		String query = "UPDATE Album SET name = \'" + name + "\', rating = " + rating + ", year = " + year + ", genre = \'" + genre + "\', type = " + album.getType() +" WHERE id = " + album.getId();
 		em.createQuery(query);
 		album.setName(name);
@@ -101,7 +80,6 @@ public class AlbumService implements Serializable {
 		if (album == null) {
 			throw new ValidationException("Invalid Album");
 		}
-		
 
 		String query = "DELETE FROM Album WHERE id = " + album.getId();
 		Query q = em.createQuery(query);
