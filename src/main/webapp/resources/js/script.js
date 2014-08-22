92aaf2ac1b3db6d5a7919692fb5f948ce966847a
Charleigh <liang.lele.3@gmail.com>
2014-08-22 15:22:53 +0100

    advanced search

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -2,3 +2,14 @@ function changeImage(element) {
      element.src = element.bln ? "resources/images/kobePlay.png" : "resources/images/kobePause.png";
      element.bln = !element.bln;  /* assigns opposite boolean value always */
  }
+
+function toggleRating(element) {
+
+    if (element.bln) {
+        $('select').find('rating').removeClass('hiddenRating');
+    }
+    else {
+        $('select').find('rating').addClass('hidden');
+    }
+    element.bln = !element.bln;
+}
