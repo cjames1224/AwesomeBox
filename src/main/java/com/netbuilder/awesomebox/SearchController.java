5682305c12189ee4b45e3ebf23f24fc09a32add3
David <dbourque4850@gmail.com>
2014-08-22 11:27:28 +0100

    added methods to searchcontroller for advanced search

--- src/main/java/com/netbuilder/awesomebox/SearchController.java
+++ src/main/java/com/netbuilder/awesomebox/SearchController.java
@@ -53,12 +53,73 @@ public class SearchController implements Serializable {
 	}
 	
 	public void searchRating(int equality,int rating) {
-		if (equality > 0) {
-			
+		if (equality == 0) {
+			this.searchService.searchSongLesserRating(rating);
+		} else if (equality == 1) {
+			this.searchService.searchSongGreaterRating(rating);
+		} else {
+			this.searchService.searchSongRating(rating);
 		}
 	}
 	
+	public void searchLength(int equality,int length) {
+		if (equality == 0) {
+			this.searchService.searchSongLesserLength(length);
+		} else if (equality == 1) {
+			this.searchService.searchSongGreaterLength(length);
+		} else {
+			this.searchService.searchSongLength(length);
+		}
+	}
 	
+	public void searchPlays(int equality,int plays) {
+		if (equality == 0) {
+			this.searchService.searchSongLesserPlays(plays);
+		} else if (equality == 1) {
+			this.searchService.searchSongGreaterPlays(plays);
+		} else {
+			this.searchService.searchSongPlays(plays);
+		}
+	}
+	
+	public void searchYear(int equality,int year) {
+		if (equality == 0) {
+			this.searchService.searchSongLesserYear(year);
+		} else if (equality == 1) {
+			this.searchService.searchSongGreaterYear(year);
+		} else {
+			this.searchService.searchSongYear(year);
+		}
+	}
+	
+	public void searchAlbumRating(int equality,int rating) {
+		if (equality == 0) {
+			this.searchService.searchSongAlbumLesserRating(rating);
+		} else if (equality == 1) {
+			this.searchService.searchSongAlbumGreaterRating(rating);
+		} else {
+			this.searchService.searchSongAlbumRating(rating);
+		}
+	}
+	
+	public void searchArtistRating(int equality,int rating) {
+		if (equality == 0) {
+			this.searchService.searchSongArtistLesserRating(rating);
+		} else if (equality == 1) {
+			this.searchService.searchSongArtistGreaterRating(rating);
+		} else {
+			this.searchService.searchSongArtistRating(rating);
+		}
+	}
+	
+	public void searchGenre(String term) {
+		this.searchService.searchSongGenre(term);
+	}
+	
+	public String search() {
+		
+		return "results";
+	}
 	
 	public String generalSearch() {
 		List<Song> list = searchService.searchSongByAlbum(searchTerm).search();
