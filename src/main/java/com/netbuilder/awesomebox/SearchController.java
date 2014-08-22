c7e19464bf79f7771289a237f2514770b7ff5bf5
David <dbourque4850@gmail.com>
2014-08-22 09:36:50 +0100

    search no longer contains duplicates

--- src/main/java/com/netbuilder/awesomebox/SearchController.java
+++ src/main/java/com/netbuilder/awesomebox/SearchController.java
@@ -1,6 +1,8 @@
 package com.netbuilder.awesomebox;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
@@ -54,6 +56,13 @@ public class SearchController implements Serializable {
 		List<Song> list = searchService.searchSongByAlbum(searchTerm).search();
 		list.addAll(searchService.searchSongByArtist(searchTerm).search());
 		list.addAll(searchService.searchSongName(searchTerm).search());
+		for (int i = 0;i < list.size();i++) {
+			for (int j = i+1;j < list.size();j++) {
+				if (list.get(i).getId() == list.get(j).getId()) {
+					list.remove(j);
+				}
+			}
+		}
 		setResults(list);
 		return "results";
 	}
