b79614d27d909c02a4c7a6d3f0b7675acef15b59
Charleigh <liang.lele.3@gmail.com>
2014-08-26 13:39:51 +0100

    half toggle code

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -3,13 +3,15 @@ function changeImage(element) {
      element.bln = !element.bln;  /* assigns opposite boolean value always */
  }
 
-function toggleRating(element) {
+function toggleRating(className) {
 
-    if (element.bln) {
-        $('select').find('rating').removeClass('hiddenRating');
-    }
-    else {
-        $('select').find('rating').addClass('hidden');
-    }
-    element.bln = !element.bln;
+	
 }
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
