fec9446d08e5c1c3daa46d8db0e95499f31e0565
Charleigh <liang.lele.3@gmail.com>
2014-08-27 13:30:37 +0100

    less ugly

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,7 +1,6 @@
 function changeImage(element) {
      element.src = element.bln ? "resources/images/kobePlay.png" : "resources/images/kobePause.png";
      element.bln = !element.bln;  /* assigns opposite boolean value always */
-     return element.src;
  }
 
 
